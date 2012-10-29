65e051e7c03377147701b8affd48bb18392b4ff9
Raoul Thill <raoul_thill@pt.lu>
2012-10-29 11:24:51 +0100

    Extending logged information

--- ldapdb/models/base.py
+++ ldapdb/models/base.py
@@ -83,7 +83,7 @@ class Model(django.db.models.base.Model):
         """
         using = using or router.db_for_write(self.__class__, instance=self)
         connection = connections[using]
-        logger.debug("Deleting LDAP entry %s" % self.dn)
+        logger.info("Deleting LDAP entry %s" % self.dn)
         connection.delete_s(self.dn)
         signals.post_delete.send(sender=self.__class__, instance=self)
 
@@ -106,7 +106,8 @@ class Model(django.db.models.base.Model):
                 if value:
                     entry.append((field.db_column, field.get_db_prep_save(value, connection=connection)))
 
-            logger.debug("Creating new LDAP entry %s" % new_dn)
+            logger.info("Creating new LDAP entry %s" % new_dn)
+            logger.debug(entry)
             connection.add_s(new_dn, entry)
 
             # update object
@@ -132,14 +133,15 @@ class Model(django.db.models.base.Model):
                 # handle renaming
                 new_dn = self.build_dn()
                 if new_dn != self.dn:
-                    logger.debug("Renaming LDAP entry %s to %s" % (self.dn, new_dn))
+                    logger.info("Renaming LDAP entry %s to %s" % (self.dn, new_dn))
                     connection.rename_s(self.dn, self.build_rdn())
                     self.dn = new_dn
             
-                logger.debug("Modifying existing LDAP entry %s" % self.dn)
+                logger.info("Modifying existing LDAP entry %s" % self.dn)
+                logger.debug(modlist)
                 connection.modify_s(self.dn, modlist)
             else:
-                logger.debug("No changes to be saved to LDAP entry %s" % self.dn)
+                logger.info("No changes to be saved to LDAP entry %s" % self.dn)
 
         # done
         self.saved_pk = self.pk
